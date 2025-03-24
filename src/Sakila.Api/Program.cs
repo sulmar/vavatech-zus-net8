1cfc9a5354d0f4907f82a07445f2f6c3d8bc2fc3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-24 15:22:40 +0100

    Dodanie secret keys i connection string

--- src/Sakila.Api/Program.cs
+++ src/Sakila.Api/Program.cs
@@ -6,13 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 string environmentName = builder.Environment.EnvironmentName;
 
 builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
 builder.Configuration.AddXmlFile("appsettings.xml");
+builder.Configuration.AddUserSecrets<Program>();
 builder.Configuration.AddCommandLine(args); // --NbpApiService:Table=B
 builder.Configuration.AddInMemoryCollection();
-
-builder.Configuration.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
-
-
 
 builder.AddFakeRepositories();
 
@@ -23,8 +21,10 @@ app.MapApi();
 
 //var level = app.Configuration["Logging:LogLevel:Microsoft.AspNetCore"];
 //var url1 = app.Configuration.GetValue<string>("NbpApiService:Url");
-var googleMapsKey = app.Configuration["GoogleMaps:AccessKey"];
+var googleMapsKey = app.Configuration["GoogleMaps"];
 Console.WriteLine(googleMapsKey);
+
+var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
 
 app.Run();
 

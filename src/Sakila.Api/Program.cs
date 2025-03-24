c347c2e5c4ee3e18edf5332f3ad3e6e388950254
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-24 12:00:26 +0100

    Dodanie przykładu obsługi HttpContext

--- src/Sakila.Api/Program.cs
+++ src/Sakila.Api/Program.cs
@@ -16,5 +16,20 @@ app.MapGet("/", homeHandler);
 app.MapGroup("/customers").MapCustomersApi();
 app.MapGroup("/orders").MapOrdersApi();
 
+
+app.Map("/test", (HttpContext context) =>
+{
+    HttpRequest request = context.Request;   
+    
+    var requestMethod = context.Request.Method;
+    var requestPath = context.Request.Path;
+    var id = context.Request.Query["id"];
+    var name = context.Request.Query["name"];
+
+    HttpResponse response = context.Response;
+});
+
+app.MapGet("/hello", (HttpRequest req, HttpResponse res) => Results.Ok("Hello World!"));
+
 app.Run();
 

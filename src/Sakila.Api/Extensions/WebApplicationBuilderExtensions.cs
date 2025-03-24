b60f298f767a0fe63b32210c08e490e311a9cb6f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-24 12:45:40 +0100

    Refaktoring: Dodanie rozszerzenia WebApplicationBuilder

---
+++ src/Sakila.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Sakila.Api.Domain.Abstractions;
+using Sakila.Api.Domain.Models;
+using Sakila.Api.Infrastructure;
+
+namespace Sakila.Api.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder AddFakeRepositories(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<IProductRepository, FakeProductRepository>();
+
+        builder.Services.AddTransient<IEnumerable<Product>>(sp =>
+            [
+                new Product { Id = 1, Name = $"Product 1", Description = "Lorem ipsum", Price = 100m },
+        new Product { Id = 2, Name = $"Product 2", Description = "Lorem ipsum", Price = 100m },
+        new Product { Id = 3, Name = $"Product 3", Description = "Lorem ipsum", Price = 100m },
+        ]
+        );
+
+        return builder;
+    }
+}
--- src/Sakila.Api/Program.cs
+++ src/Sakila.Api/Program.cs
@@ -3,18 +3,10 @@ using Sakila.Api.Domain.Abstractions;
 using Sakila.Api.Domain.Models;
 using Sakila.Api.DTO;
 using Sakila.Api.Endpoints;
-using Sakila.Api.Infrastructure;
+using Sakila.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient<IProductRepository, FakeProductRepository>();
-
-builder.Services.AddTransient<IEnumerable<Product>>(sp => 
-    [
-        new Product { Id = 1, Name = $"Product 1", Description = "Lorem ipsum", Price = 100m },
-        new Product { Id = 2, Name = $"Product 2", Description = "Lorem ipsum", Price = 100m },
-        new Product { Id = 3, Name = $"Product 3", Description = "Lorem ipsum", Price = 100m },
-    ]
-);
+builder.AddFakeRepositories();
 
 var app = builder.Build();
 

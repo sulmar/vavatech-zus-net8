151d3e7736184eb5191d2cf0acba77784cf1ea20
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-26 10:07:11 +0100

    Dodanie stopwatch middleware

--- src/Sakila.Api/Middlewares/LoggerMiddleware.cs
+++ src/Sakila.Api/Middlewares/LoggerMiddleware.cs
@@ -29,3 +29,4 @@ public class LoggerMiddleware
         Console.WriteLine($"{context.Response.StatusCode}");
     }
 }
+
---
+++ src/Sakila.Api/Middlewares/StopwatchMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace Sakila.Api.Middlewares;
+
+
+public static partial class StopwatchMiddlewareExtensions
+{
+    public static IApplicationBuilder UseStopwatch(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<StopwatchMiddleware>();
+
+        return app;
+    }
+}
+public static partial class StopwatchMiddlewareExtensions
+{
+    public class StopwatchMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<StopwatchMiddleware> logger;
+
+        public StopwatchMiddleware(RequestDelegate next, ILogger<StopwatchMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Przed wykonaniem endpointu
+            var stopwatch = Stopwatch.StartNew();
+
+            await next(context);
+
+            // Po wykonaniu endpointu
+
+            stopwatch.Stop();
+
+            logger.LogInformation("Czas wykonania: {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
+        }
+    }
+}

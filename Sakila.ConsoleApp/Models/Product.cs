05bf6d0f1e6a1fad8b70df50bd8b630bb662aa96
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-26 11:04:31 +0100

    Dodanie klienta HttpClient

---
+++ Sakila.ConsoleApp/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Sakila.ConsoleApp.Models;
+
+public record AddProductRequest
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Color { get; set; }
+    public Category Category { get; set; }
+
+    // Serializacja nie będzie zawierać pola IsSelected
+    [JsonIgnore]
+    public bool IsSelected { get; set; }
+
+}
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [JsonIgnore]    // Serializacja nie będzie zawierać listy produktów
+    public List<Product> Products { get; set; } = new();
+}

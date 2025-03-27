b2f42855e33fac458b9b691442c754f9f1fea1c1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-27 13:07:27 +0100

    Dodanie gRPC

---
+++ src/Tracking.Api/Services/MyTrackingService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+
+namespace Tracking.Api.Services;
+
+public class MyTrackingService : TrackingService.TrackingServiceBase
+{
+    private readonly ILogger<MyTrackingService> _logger;
+
+    public MyTrackingService(ILogger<MyTrackingService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<AddLocationResponse> AddLocation(AddLocationRequest request, ServerCallContext context)
+    {
+        _logger.LogInformation("PlateNumber: {PlateNumber} ({Latitude},{Longitude}) {Speed} km/h", request.PlateNumber, request.Latitude, request.Longitude, request.Speed);
+
+        var response = new  AddLocationResponse {  IsConfirmed = true };
+
+        return Task.FromResult(response);
+    }   
+}
---
+++ src/Tracking.ConsoleClient/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using Bogus;
+using Grpc.Net.Client;
+using System.Diagnostics;
+using Tracking.Api;
+
+Console.WriteLine("Hello, gRPC Client!");
+
+const string url = "https://localhost:7266";
+
+var channel = GrpcChannel.ForAddress(url);
+var client = new Tracking.Api.TrackingService.TrackingServiceClient(channel);
+
+// dotnet add package Bogus
+var faker = new Faker<AddLocationRequest>()
+    .RuleFor(p => p.PlateNumber, f => f.Vehicle.Vin())
+    .RuleFor(p => p.Latitude, f => f.Random.Float(50, 51))
+    .RuleFor(p => p.Longitude, f => f.Random.Float(27, 28))
+    .RuleFor(p=>p.Speed, f=>f.Random.Int(0, 140));
+
+var locations = faker.GenerateForever();
+
+Stopwatch stopwatch = new Stopwatch();
+
+foreach(var location in locations)
+{
+    Console.WriteLine($"Send request {location.PlateNumber} ({location.Latitude},{location.Longitude}) {location.Speed} km/h");
+
+    stopwatch.Restart();
+
+    await client.AddLocationAsync(location);
+
+    stopwatch.Stop();
+
+    Console.WriteLine($"Sent in {stopwatch.ElapsedMilliseconds}ms");
+
+    await Task.Delay(1000);
+
+}
+
+Console.WriteLine("Press any key to exit.");
+
+Console.ReadKey();
+
+

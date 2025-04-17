eff888c307c45a33ed9900ce5ef3bfcf2ad93dc8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-17 10:05:18 +0200

    Dodanie generowania tokenu JWT

--- src/Auth.Api/Infrastructure/JwtTokenService.cs
+++ src/Auth.Api/Infrastructure/JwtTokenService.cs
@@ -1,14 +1,41 @@
 ﻿using Auth.Api.Abstractions;
 using Auth.Api.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Auth.Api.Infrastructure;
 
+// dotnet add package System.IdentityModel.Tokens.Jwt
 public class JwtTokenService : ITokenService
 {
     public string CreateAccessToken(UserIdentity identity)
     {
-        // TODO: Dodać generowanie tokenu JWT
-        return "abc";
+        var claimsIdentity = new ClaimsIdentity
+         (new[]
+        {
+            new Claim(ClaimTypes.Name, identity.Username),
+            new Claim(ClaimTypes.Email, identity.Email),
+            new Claim(ClaimTypes.GivenName, identity.FirstName),
+            new Claim(ClaimTypes.Surname, identity.LastName)
+        });
+
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_long_256_bits_secret_key_your"));
+        var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        var securityToken = new JwtSecurityToken(
+            issuer: "your_issuer",
+            audience: "your_audience",
+            claims: claimsIdentity.Claims,
+            expires: DateTime.UtcNow.AddMinutes(30),
+            signingCredentials: credentials
+        );
+
+        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.WriteToken(securityToken);
+
+        return token;
     }
 }
     

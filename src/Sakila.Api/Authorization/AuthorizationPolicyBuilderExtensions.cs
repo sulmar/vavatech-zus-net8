1015c1afbc981466e2391ccc79672f5208c40078
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-17 16:12:58 +0200

    Dodanie metody rozszerzającej do sprawdzania uprawnień

---
+++ src/Sakila.Api/Authorization/AuthorizationPolicyBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Sakila.Api.Authorization
+{
+    
+    public static class AuthorizationPolicyBuilderExtensions
+    {
+        // Metoda rozszerzająca do sprawdzania uprawnień
+        public static AuthorizationPolicyBuilder RequirePermission(this AuthorizationPolicyBuilder builder, string permission)
+        {
+            builder.RequireClaim("Permission", permission);
+
+            return builder;
+        }
+    }
+}
